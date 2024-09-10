b143a232f6190f8260a5c457f6ce33ed72ea5fce
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-11 01:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //your JS code here. If required.
- // JavaScript code to run when DOM is fully loaded
-        document.addEventListener('DOMContentLoaded', function() {
-          
-			const textNode = document.createTextNode('DOM load success');
-         
-            document.body.appendChild(textNode);
+         document.addEventListener('DOMContentLoaded', function() {
+            
+            document.body.innerHTML = 'DOM load success';
         });
